444e295e716911124f8c73eaec7f548bc9faff36
NARUTO THE CODER 🐧 <163591938+Nhajjab@users.noreply.github.com>
2024-04-27 09:05:29 +0530

    Update pair1.js

--- Priyansh/commands/pair1.js
+++ Priyansh/commands/pair1.js
@@ -18,7 +18,7 @@ module.exports.onLoad = async() => {
     const dirMaterial = __dirname + `/cache/canvas/`;
     const path = resolve(__dirname, 'cache/canvas', 'pairing.jpg');
     if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.pinimg.com/736x/15/fa/9d/15fa9d71cdd07486bb6f728dae2fb264.jpg", path);
+    if (!existsSync(path)) await downloadFile(""https://i.imgur.com/zIcJeYT.jpeg",", path);
 }
 
 async function makeImage({ one, two }) {
